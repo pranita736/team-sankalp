12d0a834e5da4461451d47be75a1310379ef53c7
Praneetha Rai <59397423+pranita736@users.noreply.github.com>
2021-07-09 22:46:01 +0530

    Project

---
+++ script.js
@@ -0,0 +1,26 @@
+//get button by id
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.documentElement.scrollTop = 0; 
+}
+//Navigation bar handling
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+}
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
