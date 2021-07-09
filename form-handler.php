28ca26f4795bda36ececa05507a37c9cd6699ec8
Sumanth Kodiyalbail <48558948+sumanth2905@users.noreply.github.com>
2021-07-10 00:18:19 +0530

    Form handler using php

---
+++ form-handler.php
@@ -0,0 +1,25 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@sankalp.me';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+			  "User Email: $visitor_email.\n".
+			  "Subject: $subject.\n".
+			  "Message: $message.\n";
+
+$to = "vcetsankalp@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Replay-To $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body,$headers);
+
+header("Location: contact.html");
+?>
